01e2d4d7e2ebaeba44fbdcf4cde246bc4614ebb2
Marton Lederer <marton@lederer.hu>
2020-12-16 21:43:00 +0100

    feat: cache transaction ids that have been sent to discord

--- api/transaction.ts
+++ api/transaction.ts
@@ -9,9 +9,17 @@ const arweave = Arweave.init({
   logging: false,
 });
 
+// array of transaction IDs that are already sent to discord
+let cached: string[] = [];
+
 export default async (req: NowRequest, res: NowResponse) => {
   if (!req.query.id || typeof req.query.id !== "string")
     return res.status(403).send("Invalid request");
+
+  const transactionID: string = req.query.id;
+
+  if (cached.includes(transactionID))
+    return res.status(403).send("Already cached");
 
   return await arweave.transactions
     .get(req.query.id)
@@ -26,19 +34,19 @@ export default async (req: NowRequest, res: NowResponse) => {
       });
 
       if (!vertoTag) return res.status(403).send("Invalid tags");
-      else if (typeof req.query.id === "string") {
+      else {
         await arweave.transactions
-          .getStatus(req.query.id)
+          .getStatus(transactionID)
           .then((status) => {
             if (status.status === 200 || status.status === 400)
               return res.status(403).send("Already completed");
+
+            cached.push(transactionID);
             res.status(200).send("Sent webhook");
           })
           .catch((err) => {
             return res.status(500).send(err);
           });
-      } else {
-        return res.status(403).send("Invalid query");
       }
     })
     .catch((err) => {
