15980e683a4d0764a89623ffb9387ab47dbfd8fe
Marton Lederer <marton@lederer.hu>
2020-12-16 23:35:32 +0100

    feat: send embed
    TODO: properly check if transaction id is in cache

--- api/transaction.ts
+++ api/transaction.ts
@@ -1,5 +1,6 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 import Arweave from "arweave";
+import webhook from "webhook-discord";
 
 const arweave = Arweave.init({
   host: "arweave.net",
@@ -13,10 +14,20 @@ const arweave = Arweave.init({
 let cached: string[] = [];
 
 export default async (req: NowRequest, res: NowResponse) => {
-  if (!req.query.id || typeof req.query.id !== "string")
+  if (
+    !req.query.id ||
+    typeof req.query.id !== "string" ||
+    !req.query.from ||
+    typeof req.query.from !== "string" ||
+    !req.query.to ||
+    typeof req.query.to !== "string"
+  )
     return res.status(403).send("Invalid request");
 
-  const transactionID: string = req.query.id;
+  const transactionID: string = req.query.id,
+    from: string = req.query.from, // from amount + pst/coin
+    to: string = req.query.to, // to amount + pst/coin
+    Hook = new webhook.Webhook(process.env.WEBHOOK);
 
   if (cached.includes(transactionID))
     return res.status(403).send("Already cached");
@@ -37,12 +48,32 @@ export default async (req: NowRequest, res: NowResponse) => {
       else {
         await arweave.transactions
           .getStatus(transactionID)
-          .then((status) => {
-            if (status.status === 200 || status.status === 400)
+          .then(async (status) => {
+            /**
+             * 202 - mining
+             * 200 - mined
+             * 4xx/5xx - problem with tx
+             * 404 - might be propagating or might be permanent issue
+             */
+            if (status.status !== 202)
               return res.status(403).send("Already completed");
 
             cached.push(transactionID);
-            res.status(200).send("Sent webhook");
+
+            const discordMessage = new webhook.MessageBuilder()
+              .setColor("#b075cd")
+              .setFooter(
+                "Verto, a th8ta project",
+                "https://github.com/useverto/design/blob/master/logo/icon.png?raw=true"
+              )
+              .setTitle("New Verto Swap")
+              .setDescription(
+                `A new Verto swap has been made (**${from}** -> **${to}**)`
+              )
+              .setName("Verto");
+
+            await Hook.send(discordMessage);
+            return res.status(200).send("Sent webhook");
           })
           .catch((err) => {
             return res.status(500).send(err);
